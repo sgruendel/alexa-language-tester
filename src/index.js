cbeb366888816bb83cdb9716dc3442899b4f262b
Stefan Gründel <stefan.gruendel@gmail.com>
2021-03-05 10:54:02 +0100

    Fix i18next initialization.

--- src/index.js
+++ src/index.js
@@ -45,6 +45,11 @@ const languageStrings = {
         },
     },
 };
+i18next.use(sprintf).init({
+    overloadTranslationOptionHandler: sprintf.overloadTranslationOptionHandler,
+    resources: languageStrings,
+    returnObjects: true,
+});
 
 function getCountryRPA(slot) {
     const rpa = slot
@@ -213,12 +218,7 @@ const ErrorHandler = {
 
 const LocalizationInterceptor = {
     process(handlerInput) {
-        i18next.use(sprintf).init({
-            lng: Alexa.getLocale(handlerInput.requestEnvelope),
-            overloadTranslationOptionHandler: sprintf.overloadTranslationOptionHandler,
-            resources: languageStrings,
-            returnObjects: true,
-        });
+        i18next.changeLanguage(Alexa.getLocale(handlerInput.requestEnvelope));
 
         const attributes = handlerInput.attributesManager.getRequestAttributes();
         attributes.t = (...args) => {
